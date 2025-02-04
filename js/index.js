bd1e020f6ca7d2fdac299337a1946d6b77b137d6
Ahmed Maher <017700maher@gmail.com>
2025-02-04 15:10:34 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let countDownDate = new Date("June 14, 2025 11:00:00").getTime();
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let giorni = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let ore = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minuti = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let secondi = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("giorni").innerHTML = giorni;
+    document.getElementById("ore").innerHTML = ore;
+    document.getElementById("minuti").innerHTML = minuti;
+    document.getElementById("secondi").innerHTML = secondi;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("giorni").innerHTML = "00";
+        document.getElementById("ore").innerHTML = "00";
+        document.getElementById("minuti").innerHTML = "00";
+        document.getElementById("secondi").innerHTML = "00";
+    }
+
+},1000);
